1d1408d60d021e9841889b1dc9faae2322b98161
Lucas Antunes <lucasbh2006@gmail.com>
2015-04-13 14:52:53 -0300

    Font Open Sans Add.

--- application/views/home.php
+++ application/views/home.php
@@ -21,6 +21,8 @@
 		<link href="<?php echo base_url('includes/bootstrap/smartmenu/jquery.smartmenus.bootstrap.css'); ?>" rel="stylesheet">
 		<!-- Estilos -->
 		<link rel="stylesheet" href="<?php echo base_url('includes/css/estilos.css'); ?>">
+		<!-- Font -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('includes/webfontkit/stylesheet.css'); ?>">
 	</head>
 	<body onscroll="functionMenu()">
 		<div class="topBar">
@@ -125,7 +127,7 @@
 					<img id="imgmcode" src="<?php echo base_url('includes/imgs/app/mcode.jpg'); ?>" alt="mcode">
 				</div>
 				<div class="col-md-8">
-					<h2><strong>MOBILE CODE</strong></h2>
+					<h2><span id="bold">MOBILE CODE</span></h2>
 				  	<h4>
 				  		As soluções de MOBILE CODE da M-CODE promovem a gestão e controle do seu negócio. 
 				  	</h4>
@@ -140,7 +142,7 @@
 					<img id="imgean" src="<?php echo base_url('includes/imgs/app/ean.png'); ?>">
 				</div>
 				<div class="col-md-3">
-					<h3><strong>CODIGO EAN</strong></h3>
+					<h3><span id="bold">CODIGO EAN</span></h3>
 					<h4>EAN significa Número de Artigo Internacional. Este sistema de código de barras de 13 dígitos começou na Europa e agora é utilizado em todos os países do mundo, exceto EUA e Canadá.</h4>
 
 					<h4>Em resumo, o EAN é um código de 13 dígitos representado por barras escaneáveis, exclusivo para seu produto.</h4>
@@ -149,7 +151,7 @@
 					<img id="imgqrcode" src="<?php echo base_url('includes/imgs/app/qrcode.jpg'); ?>">
 				</div>
 				<div class="col-md-3">
-					<h3><strong>QR CODE</strong></h3>
+					<h3><span id="bold">QR CODE</span></h3>
 					<h4>Significa "Quick Response", isto é, código de resposta rápida.</h4>
 
 					<h4>Este modelo armazena ate 7.089 números ou 4.296 alfanuméricos (letras ou números). Incluindo endereços completos, informações sobre o produto no estoque e etc.</h4>
@@ -184,23 +186,23 @@
 		</div>
 		<div id="vantagens" class="item3">
 			<h2>
-				<strong>VANTAGENS PARA O USUÁRIO: </strong>
+				<span id="bold">VANTAGENS PARA O USUÁRIO: </span>
 			</h2>
 			<div class="row">
 			  <div class="col-md-4" style="margin-top: 50px;">
-			  	<h5><strong>ACOMPANHE TODO CICLO MATERIAL DE MERCHAN</strong></h5>
+			  	<h5><span id="bold">ACOMPANHE TODO CICLO MATERIAL DE MERCHAN</span></h5>
 				<center><div class="imgContainer"><img src="<?php echo base_url('includes/imgs/app/vantagem1.png'); ?>" alt="..."></div></center>
 				<h6>Acompanhe todo ciclo de vida útil do seu material de merchandising: confecção, distribuição, positivação e até a perda do material.
 				</h6>			
 			  </div>
 			  <div class="col-md-4" style="margin-top: 50px;">
-			  	<h5><strong>CONTROLE PONTOS DE VENDA</strong></h5>
+			  	<h5><span id="bold">CONTROLE PONTOS DE VENDA</span></h5>
 				<center><div class="imgContainer"><img src="<?php echo base_url('includes/imgs/app/pdv.png'); ?>" alt="..." ></div></center>
 				<h6>Controle seus pontos de venda. Saiba quais estão positivado, qual o tipo de material positivado. 
 				</h6>
 			  </div>
 			  <div class="col-md-4" style="margin-top: 50px;">
-			  	<h5><strong>CONTROLE DE EQUIPE</strong></h5>
+			  	<h5><span id="bold">CONTROLE DE EQUIPE</span></h5>
 			  	<center><div class="imgContainer"><img src="<?php echo base_url('includes/imgs/app/equipe.png'); ?>" alt="..."></div></center>
 				<h6>Acompanhe em tempo real a produtividade da sua equipe.
 				</h6>
@@ -208,19 +210,19 @@
 			</div>
 			<div class="row">
 			  <div class="col-md-4" style="margin-top: 50px;">
-			  	<h5><strong>DADOS DE CONTROLE</strong></h5>
+			  	<h5><span id="bold">DADOS DE CONTROLE</span></h5>
 				<center><div class="imgContainer"><img src="<?php echo base_url('includes/imgs/app/infos.png'); ?>" alt="..."></div></center>
 				<h6>Informações otimizadas e em tempo real de toda operação.
 				</h6>			
 			  </div>
 			  <div class="col-md-4" style="margin-top: 50px;">
-			  	<h5><strong>DASHBOARDS CUSTOMIZADOS</strong></h5>
+			  	<h5><span id="bold">DASHBOARDS CUSTOMIZADOS</span></h5>
 				<center><div class="imgContainer"><img style="margin-top: 30px;" src="<?php echo base_url('includes/imgs/app/dashboard.png'); ?>" alt="..."></div></center>
 				<h6>Controle seus pontos de venda. Saiba quais estão positivado, qual o tipo de material positivado em cada um dos PDV’s. 
 				</h6>
 			  </div>
 			  <div class="col-md-4" style="margin-top: 50px;">
-			  	<h5><strong>TEMPO DE VIDA ÚTIL</strong></h5>
+			  	<h5><span id="bold">TEMPO DE VIDA ÚTIL</span></h5>
 			  	<center><div class="imgContainer"><img src="<?php echo base_url('includes/imgs/app/checkin.png'); ?>" alt="..."></div></center>
 				<h6>Através da função check-in / check out saiba qual o tempo de vida útil do seu material. 
 				</h6>
@@ -230,7 +232,7 @@
 		<div id="controla" class="item4">
 			<div id="textoControlar">
 				<h2 style="background-color: black; color:white;border-radius:20px; padding-left:10px;">
-					<strong>O QUE CONTROLAR COM m-code: </strong>
+					<span id="bold">O QUE CONTROLAR COM m-code: </span>
 				</h2>
 			</div>
 			<div class="row">
@@ -255,7 +257,7 @@
 			</div>
 		</div>
 		<div id="clientes" class="item5">
-			<h2><strong>QUEM USA M-CODE?</strong></h2>
+			<h2><span id="bold">QUEM USA M-CODE?</span></h2>
 			<center>
 				<div class="row">
 					<div class="col-md-6">
@@ -268,14 +270,14 @@
 			</center>
 		</div>
 		<div id="empresa" class="item6">
-			<h2><strong>M-CODE</strong></h2>
+			<h2><span id="bold">M-CODE</span></h2>
 			<div class="row">
 				<div class="col-md-10">
 					<h3>A M-CODE é a empresa brasileira mais especializada em soluções de automação para leitura de códigos de barra inteligentes.
 					</h3>
 					<h3>Com sede em Belo Horizonte e unidades de negócio no Rio de Janeiro e em São Paulo a empresa atua em todo Brasil! 
 					</h3>
-					<h3><strong>VENHA TOMAR UM CAFÉ CONOSCO E SURPREENDA-SE!</strong></h3>
+					<h3><span id="bold">VENHA TOMAR UM CAFÉ CONOSCO E SURPREENDA-SE!</span></h3>
 				</div>
 				<div class="col-md-2">
 					<center><img src="<?php echo base_url('includes/imgs/app/logo.png'); ?>"></center>
@@ -283,13 +285,13 @@
 			</div>
 		</div>
 		<div id="fale" class="item7">
-			<h2><strong>ENTÃO, VAMOS TOMAR UM CAFÉ?</strong></h2>
+			<h2><span id="bold">ENTÃO, VAMOS TOMAR UM CAFÉ?</span></h2>
 			<div class="row">
 				<div class="col-md-12">
 					<h4>Gostou de nossa solução? Tem alguma dúvida ou necessita de um projeto sob medida? Entre em contato e lhe retornaremos para esclarecer dúvidas ou agendar uma visita.</h4>
 
 					<div class="row">
-						<div class="col-md-3"><h3><strong><small>FALE</small> <p>CONOSCO</p></strong></h3></div>
+						<div class="col-md-3"><h3><span id="bold"><small>FALE</small> <p>CONOSCO</p></span></h3></div>
 						<div class="col-md-9"><img src="<?php echo base_url('includes/imgs/app/cafe.png'); ?>"></div>
 					</div>
 
@@ -317,7 +319,7 @@
 					<img src="<?php echo base_url('includes/imgs/app/matriz.png'); ?>">
 				</div>
 				<div class="col-md-3">
-					<h3><strong>MATRIZ:</strong></h3>
+					<h3><span id="bold">MATRIZ:</span></h3>
 					<h4>
 						Rua Alvares Cabral, 381 – sala 1101 / 1103 – Lourdes
 						Belo Horizonte / MG
